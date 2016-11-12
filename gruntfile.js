46113044f722179229ca117bb847651aa77a87f0
Jon Weiss <playsomebeethoven@gmail.com>
2016-11-11 20:31:02 -0500

    Create gruntfile.js

---
+++ gruntfile.js
@@ -0,0 +1,22 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON("package.json"),
+        uglify: {
+            build: {
+                src: "app.js",
+                dest: "app.min.js"
+            }
+        },
+        cssmin: {
+            build: {
+                src: "app.css",
+                dest: "app.min.css"
+            }
+        }
+    });
+    
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+    grunt.loadNpmTasks("grunt-contrib-cssmin");
+    
+    grunt.registerTask("default", ["uglify", "cssmin"]);
+};
